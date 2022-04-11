d4869729b921260655bb051da952a2a5fe955c70
Brian Frahm <24798291+brian23662@users.noreply.github.com>
2022-04-11 16:38:04 -0400

    Final changes

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,13 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Laravel\Cashier\Cashier;
 
 class HomeController extends Controller
 {
     public function __invoke()
     {
         $products = Product::get();
-
+        
         return view('home', [
             'products' => $products
         ]);
