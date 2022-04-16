36da23534d27433db8a488f1b0977d66fb71a5c4
Brian Frahm <24798291+brian23662@users.noreply.github.com>
2022-04-16 09:46:04 -0400

    Updated checkout process

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cart;
 use Illuminate\Http\Request;
 
 class CheckoutController extends Controller
 {
-    public function index()
+    public function __construct()
     {
-        return view('checkout.index');
+        $this->middleware(['auth']);
+    }
+
+    public function index(Request $request)
+    {
+        $cart = Cart::bySession()->first();
+
+        $checkout = $request->user()->checkout($cart->products->pluck('stripe_id')->toArray(), [
+            'success_url' => route('orders.index'),
+            'cancel_url' => route('cart.index'),
+        ]);
+
+        return view('checkout.index', [
+            'checkout' => $checkout
+        ]);
     }
 }
