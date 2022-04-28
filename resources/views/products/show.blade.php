d10295bfcd2daeab1bb140aa0f6f26ffd1a508cb
Brian Frahm <24798291+brian23662@users.noreply.github.com>
2022-04-28 11:49:21 -0400

    Added calendar to product show blade

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -21,6 +21,17 @@
                             Add to cart
                         </x-button>
                     </form>
+                    <div class="flex items-center justify-center">
+                        <div class="datepicker relative form-floating mb-3 xl:w-96" data-mdb-toggle-button="false">
+                          <input type="text"
+                            class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                            placeholder="Select a date" />
+                          <label for="floatingInput" class="text-gray-700">Select a date</label>
+                          <button class="datepicker-toggle-button" data-mdb-toggle="datepicker">
+                            <i class="fas fa-calendar datepicker-toggle-icon"></i>
+                          </button>
+                        </div>
+                      </div>
                 </div>
             </div>
         </div>
