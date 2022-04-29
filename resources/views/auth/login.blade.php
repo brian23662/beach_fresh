68c73c0b5bb849156c4f20bf9cbf5380ba0442e4
Brian Frahm <24798291+brian23662@users.noreply.github.com>
2022-04-29 10:24:44 -0400

    Added space between register and password links on login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,11 +39,13 @@
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
             </div>
-
-            <div class="flex items-center justify-end mt-4">
+            <div class="flex items-center justify-start mt-4">
                 <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('register') }}">
-                    {{ __('Need to register?   ') }}
+                    {{ __('Need to register?') }}
                 </a>
+            </div>
+            <div class="flex items-center justify-end mt-4">
+                
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
