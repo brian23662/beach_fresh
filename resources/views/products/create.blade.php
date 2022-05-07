d5bf280360474def5a8c899f1859975e14cce82c
Brian Frahm <24798291+brian23662@users.noreply.github.com>
2022-05-07 15:32:14 -0400

    added product create

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-6">
-                    This is a future product
+                    This is a future product create page
                     {{-- <form action="{{ route('cart.products.store') }}" method="post">
                         @csrf
                         <input type="hidden" name="product_id" value="{{ $product->id }}">
