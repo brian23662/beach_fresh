5a207d137d6694ca5b60d6bbadce75d3db2b86f4
Brian Frahm <24798291+brian23662@users.noreply.github.com>
2022-05-07 22:26:50 -0400

    updated admin create product, fixed @csrf

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -8,7 +8,7 @@
     <section class="px-6 py-8">
         <x-panel class="max-w-sm mx-auto">
             <form method="POST" action="/admin/products">
-                @@csrf
+                @csrf
                 
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
@@ -25,6 +25,79 @@
                     >
 
                     @error('title')
+                        <p class="text-red-500 text-xs mt-2">{{ $messages }}</p>
+                    @enderror
+                </div>
+
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                        for="slug"
+                    >
+                        
+                    </label>
+                        Slug
+                    <input class="border border-gray-400 p-2 w-full"
+                        type="text"
+                        name="slug"
+                        id="slug"
+                        required
+                    >
+
+                    @error('slug')
+                        <p class="text-red-500 text-xs mt-2">{{ $messages }}</p>
+                    @enderror
+                </div>
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                        for="description"
+                    >
+                        Description
+                    </label>
+                
+                    <textarea class="border border-gray-400 p-2 w-full"
+                        name="description"
+                        id="description"
+                        required
+                    ></textarea>
+
+                    @error('description')
+                        <p class="text-red-500 text-xs mt-2">{{ $messages }}</p>
+                    @enderror
+
+                </div>
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                        for="stripe_id"
+                    >
+                        
+                    </label>
+                        Stripe ID
+                    <input class="border border-gray-400 p-2 w-full"
+                        type="text"
+                        name="stripe_id"
+                        id="stripe_id"
+                        required
+                    >
+
+                    @error('stripe_id')
+                        <p class="text-red-500 text-xs mt-2">{{ $messages }}</p>
+                    @enderror
+                </div>
+                <div class="mb-6">
+                    <label class="block mb-2 uppercase font-bold text-xs text-gray-700"
+                        for="file_path"
+                    >
+                        
+                    </label>
+                        Link to picture
+                    <input class="border border-gray-400 p-2 w-full"
+                        type="file_path"
+                        name="file_path"
+                        id="file_path"
+                        required
+                    >
+
+                    @error('file_path')
                         <p class="text-red-500 text-xs mt-2">{{ $messages }}</p>
                     @enderror
                 </div>
